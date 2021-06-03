f55c31fcc96c1deea9569c7a082e1c62193e962b
heondo kim <heondokim@gmail.com>
2021-06-03 13:18:35 -0700

    failed

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import {FlatList} from 'react-native'
 import {StackNavigationProp} from '@react-navigation/stack'
-import {ListItem, Avatar, Button} from 'react-native-elements'
+import {ListItem, Theme} from 'react-native-elements'
+import {ThemeProvider} from 'styled-components'
 
 import {RootStackParamList} from '../types/navigation'
-import {Container} from '../components/atoms/layout'
-import {PrimaryButton, TouchableOpacity} from '../components/atoms/button'
+import {TouchableOpacity} from '../components/atoms/button'
 
 import GROUP_DATA, {MeridianDataType} from '../data/groupData'
 
@@ -14,21 +14,37 @@ type Props = {
   navigation: MainScreenNavigationProp
 }
 
+const theme: Theme = {
+  Button: {
+    raised: true,
+  },
+  ListItemTitle: {
+    style: {
+      color: 'white',
+    },
+  },
+}
+
 export function Home(props: Props) {
   const {navigation} = props
 
   const keyExtractor = item => item.meridianID
 
   const renderItem = ({item}: {item: MeridianDataType}) => (
-    <ListItem bottomDivider>
-      <TouchableOpacity>
-        <ListItem.Content>
-          <ListItem.Title>{item.english}</ListItem.Title>
-          <ListItem.Subtitle>{item.english}</ListItem.Subtitle>
-        </ListItem.Content>
-      </TouchableOpacity>
-      <ListItem.Chevron />
-    </ListItem>
+    <ThemeProvider theme={theme}>
+      <ListItem bottomDivider containerStyle={{backgroundColor: '#23272e'}}>
+        <TouchableOpacity
+          onPress={() => {
+            navigation.navigate('PointsList', {id: item.meridianID})
+          }}>
+          <ListItem.Content>
+            <ListItem.Title>{item.english}</ListItem.Title>
+            <ListItem.Subtitle>{item.english}</ListItem.Subtitle>
+          </ListItem.Content>
+        </TouchableOpacity>
+        <ListItem.Chevron />
+      </ListItem>
+    </ThemeProvider>
   )
 
   return (
