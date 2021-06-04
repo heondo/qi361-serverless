30c749e4cf815f7800d073a202e7f1194e84c666
heondo kim <heondokim@gmail.com>
2021-06-04 12:29:33 -0700

    default user state and types, must create login and logout methods

--- src/store/user/userSlice.ts
+++ src/store/user/userSlice.ts
@@ -1,28 +1,42 @@
 import {createSlice} from '@reduxjs/toolkit'
+import {User} from '../../types/auth'
+
+export interface UserState {
+  isLoading: boolean
+  loadingMessage: boolean
+  error: string | null
+  user: null | User // TODO create user type
+}
+
+// Define the initial state using that type
+const initialState: UserState = {
+  isLoading: false,
+  loadingMessage: false,
+  error: null,
+  user: null,
+}
 
 export const userSlice = createSlice({
-  name: 'counter',
-  initialState: {
-    value: 0,
-  },
+  name: 'user',
+  initialState,
   reducers: {
     increment: state => {
       // Redux Toolkit allows us to write "mutating" logic in reducers. It
       // doesn't actually mutate the state because it uses the Immer library,
       // which detects changes to a "draft state" and produces a brand new
       // immutable state based off those changes
-      state.value += 1
+      //   state.value += 1
     },
-    decrement: state => {
-      state.value -= 1
+    login: (state, action) => {
+      console.log(state, action)
     },
     incrementByAmount: (state, action) => {
-      state.value += action.payload
+      //   state.value += action.payload
     },
   },
 })
 
 // Action creators are generated for each case reducer function
-export const {increment, decrement, incrementByAmount} = userSlice.actions
+export const {increment, login, incrementByAmount} = userSlice.actions
 
 export const userReducer = userSlice.reducer
